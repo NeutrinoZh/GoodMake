080df82c597fd908f533ce572b4476b3387249ab
NeutrinoZh <neutrinozh@gmail.com>
2022-03-22 20:53:08 +0200

    Add Configurations

--- src/main.cpp
+++ src/main.cpp
@@ -14,33 +14,52 @@ std::string get_directory() {
 }
 
 void execute(std::string cmd) {
+    std::cout << "EXECUTE '" << cmd << "'\n";
     system(cmd.c_str());
 }
 
 void make(std::string configure, std::string subdirectories, std::string flags="") {
+    std::cout << "make build configure=" + configure + " SUB_DIR=\"" + subdirectories + "\" " + (flags.empty() ? "" : ("compile_flags=" + flags)) << std::endl; 
     execute("make build configure=" + configure + " SUB_DIR=\"" + subdirectories + "\" " + (flags.empty() ? "" : ("compile_flags=" + flags)));
 }
 
 void execute_new(std::string cmd) {
-    execute("mintty " + cmd);
+    execute("mintty& " + cmd);
 }
 
-int main() {
+int main(int countArg, char* args[]) {
+    std::cout << "Please enter any key for exit\n";
+    system("read -N 1");
+
     std::cout << "GoodMake Start" << std::endl;
+
+    std::cout << "Reading config file: " << (countArg >= 2 ? args[1] : "./config") << std::endl;
+
+    std::ifstream config(countArg >= 2 ? args[1] : "./config");
     
+    std::string title = ""; std::getline(config, title);
+    std::string flags = ""; std::getline(config, flags);
+    std::string gdb = ""; std::getline(config, gdb);
+
+    title.pop_back();
+    flags.pop_back();
+
+    config.close();
+
     std::cout << "The following subdirectories were found in src:" << std::endl;
     const std::string subdirectories = get_directory();
     std::cout << (subdirectories.empty() ? "None" : subdirectories) << std::endl;
 
-    make("Debug", subdirectories, "-g"); 
-    
-    execute_new("./bin/Test/main");
+    make("Debug", subdirectories, flags); 
+
+    std::string cmd = ("./bin/" + title + "/main");
+    execute_new(cmd);
     Sleep(1000);
-    execute("ps aux | grep /cygdrive/c/Users/acer/Desktop/Cpp/GoodMake/bin/Test/main | grep -v grep > pid");
+    execute("ps aux | grep ./bin/" + title + "/main | grep -v grep > pid");
 
     std::ifstream file("./pid");
     
-    std::string pid = "";
+    std::string pid = "";   
     std::getline(file, pid);
     pid = pid.substr(5, 4);
 
@@ -48,7 +67,9 @@ int main() {
 
     file.close();   
 
-    execute_new("gdb \"./bin/Test/main\" " + pid);
+    std::cout << "GDB:" << (gdb == "True") << "\n";
+    if (gdb == "True")
+        execute_new("gdb \"./bin/" + title + "/main\" " + pid);
 
     std::cout << "Please enter any key for exit\n";
     system("read -N 1");
